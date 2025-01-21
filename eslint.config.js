1c6ceb3281984d200678b7eabedafd28af24ead9
Davide Malan <davidemalan@hotmail.it>
2025-01-21 16:43:03 +0100

    fix lint rules and packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,61 +1,71 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier/recommended';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  prettier,
+  { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: importPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-unused-expressions": [
-        "error",
-        { allowShortCircuit: true, allowTernary: true },
-      ],
-      "import/consistent-type-specifier-style": "error",
-      "import/order": [
-        "warn",
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+      'no-case-declarations': 'off',
+      'no-prototype-builtins': 'off',
+      'no-restricted-imports': [
+        'error',
         {
-          "newlines-between": "always",
-          alphabetize: {
-            order: "asc",
-            orderImportKind: "asc",
-            caseInsensitive: true,
-          },
-          pathGroups: [
+          patterns: [
             {
-              pattern: "{.,..}/**/*.{less,css,scss}",
-              patternOptions: {
-                dot: true,
-                nocomment: true,
-              },
-              group: "unknown",
-              position: "after",
+              group: ['../*'],
+              message: 'Import from a parent is restricted, use @ alias.',
             },
           ],
         },
       ],
-      "prettier/prettier": "warn",
+      'import/consistent-type-specifier-style': 'error',
+      'import/order': [
+        'warn',
+        {
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            orderImportKind: 'asc',
+            caseInsensitive: true,
+          },
+          pathGroups: [
+            {
+              pattern: '{.,..}/**/*.{less,css,scss}',
+              patternOptions: {
+                dot: true,
+                nocomment: true,
+              },
+              group: 'unknown',
+              position: 'after',
+            },
+          ],
+        },
+      ],
+      'prettier/prettier': 'warn',
     },
   }
 );
