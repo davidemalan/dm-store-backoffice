edbe198af8ea20e1aacc5658e67619ed68f6797d
Davide Malan <davidemalan@hotmail.it>
2025-01-21 14:40:32 +0100

    base structure, sass, eslint

---
+++ eslint.config.js
@@ -0,0 +1,61 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-expressions": [
+        "error",
+        { allowShortCircuit: true, allowTernary: true },
+      ],
+      "import/consistent-type-specifier-style": "error",
+      "import/order": [
+        "warn",
+        {
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            orderImportKind: "asc",
+            caseInsensitive: true,
+          },
+          pathGroups: [
+            {
+              pattern: "{.,..}/**/*.{less,css,scss}",
+              patternOptions: {
+                dot: true,
+                nocomment: true,
+              },
+              group: "unknown",
+              position: "after",
+            },
+          ],
+        },
+      ],
+      "prettier/prettier": "warn",
+    },
+  }
+);
