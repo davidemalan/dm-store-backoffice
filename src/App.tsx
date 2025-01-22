37c000988637346bc516ddbe91d2a66a936aa59b
Davide Malan <davidemalan@hotmail.it>
2025-01-22 20:16:33 +0100

    main styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 
-import reactLogo from './assets/react.svg';
+import useAxios from './hooks/useAxios';
+import { getStoreById } from './services/api';
 
-import './App.scss';
+const App: React.FC = () => {
+  const { data, error, isLoading, apiWrapper } = useAxios();
 
-function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log('loading', isLoading);
+  }, [isLoading]);
 
-  return (
-    <>
-      <div>
-        <a
-          href="https://react.dev"
-          target="_blank"
-        >
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
-}
+  useEffect(() => {
+    console.log('data', data);
+  }, [data]);
+
+  useEffect(() => {
+    apiWrapper(getStoreById);
+    // apiWrapper(() => getProductById('blallo'));
+  }, [apiWrapper]);
+
+  if (isLoading) return <>loading...</>;
+
+  if (error) return <>{error}</>;
+
+  return <>pippo</>;
+};
 
 export default App;
