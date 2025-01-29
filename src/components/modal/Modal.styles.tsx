8ed8d42d6177f4474593693d68bab81ebed87490
Davide Malan <davidemalan@hotmail.it>
2025-01-29 18:06:49 +0100

    added modal fields

--- src/components/modal/Modal.styles.tsx
+++ src/components/modal/Modal.styles.tsx
@@ -1,14 +1,20 @@
 import { styled } from 'styled-components';
 
+import Button from '../button/Button';
+
 const Modal = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(0, 0, 0, 0.8);
-  position: absolute;
+  background-color: rgba(0, 0, 0, 0.6);
+  position: fixed;
   inset: 0;
+
+  section {
+    margin-top: 20px;
+  }
 `;
 
 const Card = styled.div`
@@ -16,9 +22,31 @@ const Card = styled.div`
   padding: 20px 40px;
   background: #fff;
   text-align: center;
+  border-radius: 4px;
+  max-width: 750px;
+  overflow-y: auto;
+  max-height: 100vh;
 
   ${({ theme }) => theme.breakpoints.up('md')} {
+    max-height: 90vh;
     min-width: 500px;
+  }
+
+  /* style scrollbar to make it bit smaller */
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #c4c4c4;
+
+    &:hover {
+      background-color: #acacac;
+    }
   }
 `;
 
@@ -26,8 +54,70 @@ const CardHeader = styled.div`
   text-align: right;
 `;
 
+const TextInput = styled.input`
+  width: 100%;
+  height: 40px;
+  padding: 10px 10px;
+  font-size: 16px;
+  color: #495057;
+  background-color: #fff;
+  border: 1px solid #ced4da;
+  border-radius: 4px;
+  margin: 10px 0;
+
+  transition:
+    border-color 0.15s ease-in-out,
+    box-shadow 0.15s ease-in-out;
+
+  &:focus {
+    border-color: #80bdff;
+    outline: 0;
+    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+  }
+
+  ${({ theme }) => theme.breakpoints.up('md')} {
+    width: calc(50% - 20px);
+    margin-right: 20px;
+  }
+`;
+
+const PriceLabel = styled.label`
+  font-weight: 700;
+`;
+
+const PriceInput = styled(TextInput)`
+  margin-left: 20px;
+`;
+
+const LongInput = styled(TextInput)`
+  width: 100%;
+`;
+
+const AddButton = styled(Button)``;
+
+const SubmitButton = styled.input`
+  color: #fff;
+  background-color: #2ecc71;
+  border-radius: 25px;
+  padding: 10px 30px;
+  margin-bottom: 14px;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+
+  &:hover {
+    background-color: #25a85c;
+  }
+`;
+
 export default {
   Modal,
+  TextInput,
+  PriceLabel,
+  PriceInput,
+  LongInput,
   Card,
   CardHeader,
+  AddButton,
+  SubmitButton,
 };
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,6 +1,9 @@
-import { FC } from 'react';
+import { ChangeEvent, FC, FormEvent, ReactElement, useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
+import Button from '../button/Button';
 import { Close } from '../icons/Close';
+import { Plus } from '../icons/Plus';
 
 import Styled from './Modal.styles';
 
@@ -8,16 +11,162 @@ interface ModalProps {
   closeModal: () => void;
 }
 
-const Modal: FC<ModalProps> = ({ closeModal }): React.ReactElement => {
+const Modal: FC<ModalProps> = ({ closeModal }): ReactElement => {
+  const [newProduct, setNewProduct] = useState({
+    productId: uuidv4(),
+    title: '',
+    category: '',
+    price: 0.01,
+    employee: '',
+    description: '',
+    reviews: [''],
+  });
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log('handleSubmit', newProduct);
+  };
+
+  // on change input update state based on input name
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setNewProduct((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  // on change input update state based on input name
+  const handleReviewChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    const index = parseInt(name.split('_')[1], 10);
+
+    // new array to update state
+    const updatedReviews = [...newProduct.reviews];
+    updatedReviews[index] = value;
+
+    setNewProduct((prev) => ({
+      ...prev,
+      reviews: updatedReviews,
+    }));
+  };
+
+  // add review space
+  const addReview = (event: FormEvent<HTMLFormElement>) => {
+    // prevent default triggering of submit
+    event.preventDefault();
+
+    const newReviews = [...newProduct.reviews, ''];
+
+    setNewProduct({
+      ...newProduct,
+      reviews: newReviews,
+    });
+  };
+
+  // remove scroll when opened
+  useEffect(() => {
+    document.body.style.overflowY = 'hidden';
+
+    return () => {
+      document.body.style.overflowY = 'unset';
+    };
+  }, []);
+
   return (
     <Styled.Modal>
       <Styled.Card>
+        {/* close button */}
         <Styled.CardHeader>
           <button onClick={closeModal}>
             <Close />
           </button>
         </Styled.CardHeader>
-        <h3>Add new Product</h3>
+        <h1>Add new Product</h1>
+
+        <form onSubmit={handleSubmit}>
+          {/* simple inputs section */}
+          <section>
+            <Styled.TextInput
+              name="employee"
+              value={newProduct.employee}
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Employee"
+            />
+            <Styled.TextInput
+              name="category"
+              value={newProduct.category}
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Category"
+            />
+            <Styled.TextInput
+              name="title"
+              value={newProduct.title}
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Title"
+            />
+          </section>
+
+          <section>
+            {/* description long input */}
+            <Styled.LongInput
+              name="description"
+              value={newProduct.description}
+              onChange={handleInputChange}
+              placeholder="Description"
+            />
+
+            {/* price input with label because of no placeholder */}
+            <Styled.PriceLabel>
+              Price:
+              <Styled.PriceInput
+                name="price"
+                value={newProduct.price}
+                onChange={handleInputChange}
+                type="number"
+                placeholder="Price"
+                step=".01"
+                min="0.01"
+              />
+            </Styled.PriceLabel>
+          </section>
+
+          {/* reviews */}
+          <section>
+            <h4>Reviews:</h4>
+            {newProduct.reviews.map((review, i) => (
+              <Styled.LongInput
+                key={`review_${i}`}
+                name={`review_${i}`}
+                value={review}
+                onChange={handleReviewChange}
+                type="text"
+                placeholder={`Review ${i + 1}`}
+              />
+            ))}
+            <Button
+              backgroundColor="#ededed"
+              rounded
+              onClick={(event) => addReview(event as FormEvent<HTMLFormElement>)}
+            >
+              <Plus title="Add review" />
+            </Button>
+          </section>
+
+          {/* submit input not Button for accessibility */}
+          <section>
+            <Styled.SubmitButton
+              type="submit"
+              value="Add Product"
+            />
+          </section>
+        </form>
       </Styled.Card>
     </Styled.Modal>
   );
