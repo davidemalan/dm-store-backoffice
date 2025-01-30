3cbd82fe0cf8116e7f3c7cc0ce5e0ecc1031b4d1
Davide Malan <davidemalan@hotmail.it>
2025-01-30 15:37:40 +0100

    stats menu

---
+++ src/routes/Stats.styles.tsx
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+const StatsHeaderWrapper = styled.div`
+  width: 100%;
+  text-align: end;
+`;
+
+export default {
+  StatsHeaderWrapper,
+};
--- src/routes/Stats.tsx
+++ src/routes/Stats.tsx
@@ -1,7 +1,17 @@
 import { FC, ReactElement } from 'react';
+import { useTheme } from 'styled-components';
+import { useMediaQuery } from 'usehooks-ts';
+
+import MobileMenu from '../components/mobileMenu/MobileMenu';
+
+import Styled from './Stats.styles';
 
 const Stats: FC = (): ReactElement => {
-  return <div>Stats</div>;
+  const { up } = useTheme().breakpoints;
+  // replace media because the up function returns it as part of the string
+  const isMd = useMediaQuery(up('md').replace('@media ', ''));
+
+  return <Styled.StatsHeaderWrapper>{!isMd && <MobileMenu />}</Styled.StatsHeaderWrapper>;
 };
 
 export default Stats;
